b04e2230e9f08eb8ce87823899014dec573ff2d1
Benjamin Pannell <benjamin@pannell.dev>
2019-04-16 20:55:21 +0100

    Initial commit

---
+++ DisableStandby/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DisableStandby
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            bool Disabled = false;
+
+            NotifyIcon ico = new NotifyIcon();
+            ico.Text = "Standby Inhibitor";
+            ico.Icon = DisableStandby.Properties.Resources.Battery_Options;
+            ico.ContextMenuStrip = new ContextMenuStrip();
+
+            ico.ContextMenuStrip.Items.Add("Enable Standby");
+            ico.ContextMenuStrip.Items[0].Click += (o, e) =>
+                {
+                    Disabled = !fnEnableStandby(ico);
+                };
+
+            ico.ContextMenuStrip.Items.Add("Disable Standby");
+            ico.ContextMenuStrip.Items[1].Click += (o, e) =>
+                {
+                    Disabled = fnDisableStandby(ico);
+                };
+
+            ico.ContextMenuStrip.Items.Add(new ToolStripSeparator());
+
+            ico.ContextMenuStrip.Items.Add("Exit");
+            ico.ContextMenuStrip.Items[3].Click += (o, e) =>
+                {
+                    Application.Exit();
+                };
+
+            ico.Click += (o, e) =>
+                {
+                    if (Disabled)
+                    {
+                        ico.BalloonTipIcon = ToolTipIcon.Info;
+                        ico.BalloonTipTitle = "Standby Disabled";
+                        ico.BalloonTipText =
+                            "Standby is currently disabled on your computer.";
+
+                        ico.ShowBalloonTip(2000);
+                    }
+                    else
+                    {
+                        ico.BalloonTipIcon = ToolTipIcon.Info;
+                        ico.BalloonTipTitle = "Standby Enabled";
+                        ico.BalloonTipText =
+                            "Standby is currently enabled on your computer.";
+
+                        ico.ShowBalloonTip(2000);
+                    }
+                };
+
+            ico.Visible = true;
+
+            Disabled = fnDisableStandby(ico);
+
+            Application.ApplicationExit += (o, e) =>
+                {
+                    Disabled = !fnEnableStandby(ico);
+                };
+
+            Application.Run();
+        }
+
+        static bool fnDisableStandby(NotifyIcon ico)
+        {
+            bool res = SierraLib.Win32API.SystemPowerState.PreventStandby();
+            if (!res)
+            {
+                ico.BalloonTipIcon = ToolTipIcon.Error;
+                ico.BalloonTipTitle = "Error Disabling Standby";
+                ico.BalloonTipText =
+                    "There was a problem disabling Standby on your computer\n" +
+                    "Please make sure that you are running this application on Windows XP Service Pack 3 or higher.";
+
+                ico.ShowBalloonTip(2000);
+            }
+            else
+            {
+                ico.BalloonTipIcon = ToolTipIcon.Info;
+                ico.BalloonTipTitle = "Standby Disabled";
+                ico.BalloonTipText =
+                    "Standby has been successfully disabled on your computer.";
+
+                ico.ShowBalloonTip(2000);
+            }
+
+            return res;
+        }
+
+        static bool fnEnableStandby(NotifyIcon ico)
+        {
+            bool res = SierraLib.Win32API.SystemPowerState.Reset();
+            if (!res)
+            {
+                ico.BalloonTipIcon = ToolTipIcon.Error;
+                ico.BalloonTipTitle = "Error Enabling Standby";
+                ico.BalloonTipText =
+                    "There was a problem enabling Standby on your computer\n" +
+                    "Please make sure that you are running this application on Windows XP Service Pack 3 or higher.";
+
+                ico.ShowBalloonTip(2000);
+            }
+            else
+            {
+                ico.BalloonTipIcon = ToolTipIcon.Info;
+                ico.BalloonTipTitle = "Standby Enabled";
+                ico.BalloonTipText =
+                    "Standby has been successfully re-enabled on your computer.";
+
+                ico.ShowBalloonTip(2000);
+            }
+
+            return res;
+        }
+    }
+}
---
+++ DisableStandby/SystemPowerState.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SierraLib.Win32API
+{
+    public static class SystemPowerState
+    {
+        [Flags]
+        public enum ExecutionState : uint
+        {
+            /// <summary>
+            /// Prevents the system from entering sleep/low power mode.
+            /// </summary>
+            SystemRequired = 0x1,
+
+            /// <summary>
+            /// Prevents the display from entering sleep/low power mode.
+            /// </summary>
+            DisplayRequired = 0x2,
+
+            /// <summary>
+            /// Notifies the OS that the current application requires
+            /// the system to continue operation while imitating a
+            /// sleep/low power mode.
+            /// </summary>
+            AwayModeRequired = 0x40,
+
+            /// <summary>
+            /// Resets any present flags if used alone or enables the selected
+            /// flag until <see cref="Reset"/> is called.
+            /// </summary>
+            Continuous = 0x80000000,
+
+            UserPresent = 0x4
+        }
+
+        [DllImport("kernel32.dll")]
+        public static extern uint SetThreadExecutionState(ExecutionState executionFlags);
+
+        public static bool PreventStandby()
+        {
+            return SetExecutionState(ExecutionState.SystemRequired | ExecutionState.Continuous);
+        }
+
+        public static bool Reset()
+        {
+            return SetExecutionState(ExecutionState.Continuous);
+        }
+
+        public static bool PreventScreenStandby()
+        {
+            return SetExecutionState(ExecutionState.DisplayRequired | ExecutionState.Continuous);
+               
+        }
+
+        public static bool SetExecutionState(ExecutionState flags)
+        {
+            uint res = SetThreadExecutionState(flags);
+
+            if (res == 0)
+                return false;
+            return true;
+
+        }
+    }
+}
